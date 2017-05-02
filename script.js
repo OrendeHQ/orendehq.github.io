cc8bec3560afd52b2747b0ab16f7ad6e2f3da9ba
Stanley Nguyen <hung.ngn.the@gmail.com>
2017-05-03 04:41:52 +0800

    [Resolves] First Version WIP hero section
    WIP projects section
    
    WIP main page
    
    WIP menu animation

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(document).on('mouseover', '.tree .tree__body li', function() {
+    var $target = $(event.target);
+    if ($target.hasClass('active')) return;
+    $('.tree .tree__body li').removeClass('active');
+    $target.addClass('active');
+    var $tab = $('#' + $target.data('tab'));
+    $('.long-description').removeClass('active');
+    $tab.addClass('active');
+  });
+  
+  $(document).on('scroll', function() {
+    var position = $(window).scrollTop();
+    if (position > $(window).innerHeight()) $('.nav').addClass('active');
+    else $('.nav').removeClass('active');
+  });
+
+  $(document).on('click', '#toggleMenu', function() {
+    $('#toggleMenu .divider').addClass('active');
+    $('.navbar-menu').addClass('active');
+    $('.navbar-menu .close').addClass('active');
+  });
+  $(document).on('click', '.navbar-menu .close', function() {
+    $('#toggleMenu .divider').removeClass('active');
+    $('.navbar-menu').removeClass('active');
+    $('.navbar-menu .close').removeClass('active');
+  });
+});
